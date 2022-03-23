8c91df6ab53421138a26d98d4d6282cb81ff2582
Daniel Corredor Acosta <dann95.moz@gmail.com>
2022-03-22 19:25:38 -0500

    redesigned:landing; routes;components;aimsregistering

--- src/components/LoggedAims.js
+++ src/components/LoggedAims.js
@@ -1,8 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'
-=======
->>>>>>> ba57a0b1547f3986e1601f0c8624ed3577530489
 import { Container, ContainerP, CustomButton, DivAims, H5 } from '../styled/LoggedHome'
 import NewAims from './NewAims'
 
@@ -14,24 +11,15 @@ const LoggedAims = () => {
             <SaldoCuentas />
             <ContainerP>
                 <H5>Mis cuentas </H5>
-<<<<<<< HEAD
                 <center>
-=======
-                <div>
->>>>>>> ba57a0b1547f3986e1601f0c8624ed3577530489
                     <h2>Aun no tienes ninguna cuenta</h2>
                     <DivAims>
                         <NewAims />
                     </DivAims>
-<<<<<<< HEAD
 
                     <Link to={"/addnewaim"}><CustomButton className='text-light mt-5'><b>Agregar una cuenta</b></CustomButton>
                     </Link>
                 </center>
-=======
-                </div>
-                <CustomButton className='text-light mt-5'><b>Agregar una cuenta</b></CustomButton>
->>>>>>> ba57a0b1547f3986e1601f0c8624ed3577530489
             </ContainerP>
         </Container>
 
