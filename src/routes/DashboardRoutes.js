338830ca9ce129d45dd4b9a2cc6e1f47ba329c17
Daniel Corredor Acosta <dann95.moz@gmail.com>
2022-03-25 09:19:15 -0500

    cambio de icono de tips

--- src/routes/DashboardRoutes.js
+++ src/routes/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import AddNewAim from '../components/AddNewAim'
 import LabelBottomNavigation from '../components/LabelBottomNavigation'
 import LoggedAccounts from '../components/LoggedAccounts'
+import LoggedAims from '../components/LoggedAims'
 import LoggedHome from '../components/LoggedHome'
 import LoggedProfile from '../components/LoggedProfile'
 import PlusAim from '../components/PlusAim'
@@ -12,19 +13,20 @@ import YourCustomAim from '../components/YourCustomAim'
 const DashboardRoutes = () => {
   return (
     <>
-        <div>
+      <div>
         <LabelBottomNavigation />
-            <Routes>
-                <Route exact path="/loggedhome" element={<LoggedHome />}/>
-                <Route exact path="/loggedprofile" element={<LoggedProfile />}/>
-                <Route exact path="/loggedacc" element={<LoggedAccounts />}/>                
-                <Route exact path="/plusaim" element={<PlusAim />}/>
-                <Route exact path="/yourcustomaim" element={<YourCustomAim />}/>
-                <Route exact path="/plusaimtype" element={<PlusAimType />}/>         
-                <Route exact path="/addnewaim" element={<AddNewAim />}/>
-                <Route path='*' element={<Navigate to='/loggedhome' />} />
-            </Routes>
-        </div>
+        <Routes>
+          <Route exact path="/loggedhome" element={<LoggedHome />} />
+          <Route exact path="/loggedprofile" element={<LoggedProfile />} />
+          <Route exact path="/loggedacc" element={<LoggedAccounts />} />
+          <Route exact path="/plusaim" element={<PlusAim />} />
+          <Route exact path="/loggedaims" element={<LoggedAims />} />
+          <Route exact path="/yourcustomaim" element={<YourCustomAim />} />
+          <Route exact path="/plusaimtype" element={<PlusAimType />} />
+          <Route exact path="/addnewaim" element={<AddNewAim />} />
+          <Route path='*' element={<Navigate to='/loggedhome' />} />
+        </Routes>
+      </div>
     </>
   )
 }
