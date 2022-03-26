adb60f88d79c3957cbfcaf46f62b22e9db03cc0e
Laura Berrio <blauritab@gmail.com>
2022-03-26 12:33:28 -0500

    store con aims

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,6 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk';
+import { aimsReducer } from '../reducers/aimsReducer';
 import { loginReducer } from '../reducers/loginReducer';
 import { registerReducer } from '../reducers/registerReducer';
 
@@ -8,8 +9,8 @@ window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
     login: loginReducer,
-    register: registerReducer
-    
+    register: registerReducer,
+    aims: aimsReducer
 })
 export const store = createStore(
     reducers,
