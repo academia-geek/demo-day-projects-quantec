d51cb5133189b7be1784715a09a2bcc138843976
Daniel Corredor Acosta <dann95.moz@gmail.com>
2022-03-30 18:23:09 -0500

    rediseño a la card de objetivos

--- src/components/NewAims.js
+++ src/components/NewAims.js
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deleteAimsAsyn, filterAimsAsyn, listAimsAsyn } from '../redux/actions/aimsAction';
 import { CustomButton } from '../styled/LoggedHome';
-import { Ahorro, Aims, ContainerAims, DivInfo, Final, H3, Inicial, Meta, MiAhorro, Progress, Values, VerMas } from '../styled/NewAimsStyled';
+import { Ahorro, Aims, ContainerAims, DivInfo, DivInfoS, Final, H3, Inicial, Meta, MiAhorro, Progress, Values, VerMas } from '../styled/NewAimsStyled';
 import "../styles/newAim.css"
+import DeleteForever from '@mui/icons-material/DeleteForever';
 const NewAims = () => {
 
     const { aims } = useSelector(store => store.aims)
@@ -23,11 +24,30 @@ const NewAims = () => {
                 {
                     aims.map((a, index) => (
                         <ContainerAims key={index}>
-                            <DivInfo>
-                                <H3>{a.aim}</H3>
+                            <DivInfoS>
+                                <DivInfo>
+                                    <H3>
+                                        {a.aim}
+                                    </H3>
+                                    <img src="https://res.cloudinary.com/dn1jeryp3/image/upload/v1648680087/proyecto-final/clarity_edit-line_x2nsaf.svg" alt="" />
 
-                                <VerMas to='/vermas' state={{ id: a.idDocument }}>Ver más</VerMas>
-                            </DivInfo>
+                                </DivInfo>
+
+
+                                <VerMas to='/vermas'
+                                    state={{ id: a.idDocument }}>
+                                    <DivInfo>
+
+
+
+
+                                        <img src="https://res.cloudinary.com/dn1jeryp3/image/upload/v1648680089/proyecto-final/clarity_trash-line_ihgzqm.svg" alt="" />
+                                        Eliminar
+
+                                    </DivInfo>
+                                </VerMas>
+                            </DivInfoS>
+
                             <div className='mt-3'>
                                 <Values>
                                     <MiAhorro>Mi ahorro</MiAhorro>
--- src/styled/NewAimsStyled.js
+++ src/styled/NewAimsStyled.js
@@ -13,16 +13,23 @@ width:327px;
     margin-bottom: 3%;
     
 `
+export const DivInfoS = styled.div`
+display:flex;
+justify-content:space-between;
+margin:20px 9px 0px 9px;
+align-items:center;
+`
 export const DivInfo = styled.div`
-    display: flexbox;
-    position: relative;
-    
+display:flex; 
+align-items: flex-end;
 `
 export const H3 = styled.h3`
     color: #757575;
     font-size: 16px;
     font-weight: 700;
-    margin: 7% 0 1% 6%;
+    margin: 0;
+    width:70px;
+    
 `
 export const Aims = styled.h6`
     border: 1px solid #31D3FB;
@@ -36,12 +43,14 @@ export const Aims = styled.h6`
 `
 export const VerMas = styled(Link)`
     text-decoration: none;
-    color: #3C1280;
-    font-size: 12px;
-    margin-top: 7%;
-    position: absolute;
-    left: 78%;
-    font-weight: 600;
+    font-weight: 400;
+font-size: 12px;
+line-height: 15px;
+
+/* identical to box height */
+letter-spacing: -0.3px;
+
+color: rgba(153, 150, 150, 0.7);
 `
 export const Ahorro = styled.span`
     color: #757575;
@@ -56,8 +65,17 @@ export const Meta = styled.span`
     margin-left: 52%;
 `
 export const MiAhorro = styled.span`
- color: #757575;
-    font-size: 12px;
+ont-style: normal;
+font-weight: 600;
+font-size: 12px;
+line-height: 15px;
+
+/* identical to box height */
+letter-spacing: -0.3px;
+
+color: #757575;
+
+
 `
 export const Progress = styled(ProgressBar)`
    
